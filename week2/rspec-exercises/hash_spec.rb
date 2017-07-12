b935ec276dce51c897e7d525f3b37ac352799670
Issei Morimoto <issei.morimoto@gmail.com>
2017-07-12 13:55:10 +0200

    Added rspec exercices

---
+++ week2/rspec-exercises/hash_spec.rb
@@ -0,0 +1,12 @@
+describe Hash do
+	before do
+		@hash = Hash.new({:hello => 'world'})
+	end
+	it "should return a blank instance" do
+		Hash.new.should == {}
+	end
+
+	it "hash the correct information in a key" do
+		@hash[:hello].should == 'world'
+	end
+end
